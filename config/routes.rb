7dd34f6918cefc05a517a2509abe51dfb2438bcc
Ychan0114 <ychan@nagaharawaichinoMacBook-puro.local>
2019-02-09 20:08:27 +0900

    Add_user-sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :messages, only: [:show, :new, :create]
+  resources :users, only: [:edit, :update]
 
     root :to => 'messages#index'
 end
