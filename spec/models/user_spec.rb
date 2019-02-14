8f3a57e02577b29d1af5aa81b1a03c5443f02084
Ychan0114 <ychan@nagaharawaichinoMacBook-puro.local>
2019-02-14 21:57:45 +0900

    Add-email-test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,10 @@ describe User do
        user.valid?
        expect(user.errors[:name]).to include("を入力してください")
     end
+    it "is invalid without a email" do
+      user = User.new(name: "test", email: "", password: "00000000", password_confirmation: "00000000")
+       user.valid?
+       expect(user.errors[:email]).to include("を入力してください")
+    end
   end
 end
