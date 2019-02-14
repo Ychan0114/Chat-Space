e17d07a9b8e5300b7cbabc36a47638c4e5536056
Ychan0114 <ychan@nagaharawaichinoMacBook-puro.local>
2019-02-14 23:17:04 +0900

    Add-user-test-already-email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,5 +15,11 @@ describe User do
        user.valid?
        expect(user.errors[:email]).to include("を入力してください")
     end
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("はすでに存在します")
+    end
   end
 end
