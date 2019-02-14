a92734d6d2b5f14bce989fa0959f54aba416a2ed
Ychan0114 <ychan@nagaharawaichinoMacBook-puro.local>
2019-02-14 23:21:11 +0900

    Add-user-test-password_confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,8 +17,13 @@ describe User do
     end
     it "is invalid without a password" do
       user = build(:user, password:"")
-       user.valid?
-       expect(user.errors[:password]).to include("を入力してください")
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+    it "is invalid without a password_confirmation" do
+      user = build(:user, password_confirmation:"")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とPasswordの入力が一致しません")
     end
     it "is invalid with a duplicate email address" do
       user = create(:user)
