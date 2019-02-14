7dd90ac7f445f7fa8bc595ba5ab922c3e106e59b
Ychan0114 <ychan@nagaharawaichinoMacBook-puro.local>
2019-02-14 23:18:48 +0900

    Add-user-test-password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,11 @@ describe User do
        user.valid?
        expect(user.errors[:email]).to include("を入力してください")
     end
+    it "is invalid without a password" do
+      user = build(:user, password:"")
+       user.valid?
+       expect(user.errors[:password]).to include("を入力してください")
+    end
     it "is invalid with a duplicate email address" do
       user = create(:user)
       another_user = build(:user)
