3b73c85294409a5a62e9e3e7af43895adbdfb111
Ychan0114 <ychan@nagaharawaichinoMacBook-puro.local>
2019-02-18 19:58:59 +0900

    Add-sendi message scrollBottom

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -33,6 +33,7 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       $('.form__message').val('');
     })
   });
