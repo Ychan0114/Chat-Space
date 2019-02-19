1a116680a073e7b0e94c6771409f1dd40491b232
Ychan0114 <ychan@nagaharawaichinoMacBook-puro.local>
2019-02-19 20:45:49 +0900

    Add $.ajax

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,12 @@
 $(function() {
   $(".group-form__input").on("keyup", function() {
     var input = $(".group-form__input").val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    });
   });
 });
