a365a08f46e251aea6a23c5ca29fc19a8180abdc
Ychan0114 <ychan@nagaharawaichinoMacBook-puro.local>
2019-02-22 21:34:52 +0900

    Add where 'group_id'

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.json { @new_message = Message.where('id > ?', params[:message][:id]) }
+      format.json { @new_message = Message.where('id > ?', params[:message][:id]).where('group_id = ?', params[:group_id]) }
     end
   end
 
